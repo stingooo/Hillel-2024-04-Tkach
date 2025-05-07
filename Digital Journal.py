7eb67d9c19ae59319022e3ac4b0aec3e3f58c2f8
stingooo <sting10111989@gmail.com>
2025-05-08 02:02:51 +0300

    Update

--- Digital Journal.py
+++ Digital Journal.py
@@ -1,210 +1,194 @@
 from fuzzywuzzy import fuzz
-
-storage: list[dict] = [
-    {
-        "id": 1,
-        "name": "Alice Johnson",
-        "marks": [7, 8, 9, 10, 6, 7, 8],
-        "info": "Alice Johnson is 18 y.o. Interests: math",
-    },
-    {
-        "id": 2,
-        "name": "Michael Smith",
-        "marks": [6, 5, 7, 8, 7, 9, 10],
-        "info": "Michael Smith is 19 y.o. Interests: science",
-    },
-    {
-        "id": 3,
-        "name": "Emily Davis",
-        "marks": [9, 8, 8, 7, 6, 7, 7],
-        "info": "Emily Davis is 17 y.o. Interests: literature",
-    },
-    {
-        "id": 4,
-        "name": "James Wilson",
-        "marks": [5, 6, 7, 8, 9, 10, 11],
-        "info": "James Wilson is 20 y.o. Interests: sports",
-    },
-    {
-        "id": 5,
-        "name": "Olivia Martinez",
-        "marks": [10, 9, 8, 7, 6, 5, 4],
-        "info": "Olivia Martinez is 18 y.o. Interests: art",
-    },
-    {
-        "id": 6,
-        "name": "Emily Davis",
-        "marks": [4, 5, 6, 7, 8, 9, 10],
-        "info": "Daniel Brown is 19 y.o. Interests: music",
-    },
-    {
-        "id": 7,
-        "name": "Sophia Taylor",
-        "marks": [11, 10, 9, 8, 7, 6, 5],
-        "info": "Sophia Taylor is 20 y.o. Interests: physics",
-    },
-    {
-        "id": 8,
-        "name": "William Anderson",
-        "marks": [7, 7, 7, 7, 7, 7, 7],
-        "info": "William Anderson is 18 y.o. Interests: chemistry",
-    },
-    {
-        "id": 9,
-        "name": "Isabella Thomas",
-        "marks": [8, 8, 8, 8, 8, 8, 8],
-        "info": "Isabella Thomas is 19 y.o. Interests: biology",
-    },
-    {
-        "id": 10,
-        "name": "Benjamin Jackson",
-        "marks": [9, 9, 9, 9, 9, 9, 9],
-        "info": "Benjamin Jackson is 20 y.o. Interests: history",
-    },
-]
+import csv
+from pathlib import Path
 
 
-def generate_id() -> int:
-    students_ids = [student["id"] for student in storage]
+STORAGE_FILE_NAME = Path(__file__).parent / "students.csv"
+
+
+class Repository:
+    def __init__(self):
+        with open(STORAGE_FILE_NAME, "r") as file:
+            self.file = file
+            self.students = self.get_storage()
+
+    def get_storage(self):
+        reader = csv.DictReader(self.file, fieldnames=["id", "name", "marks", "info"], delimiter=";")
+        return list(reader)
+
+    def update_storage(self):
+        with open(STORAGE_FILE_NAME, "w") as file:
+            writer = csv.DictWriter(file, fieldnames=["id", "name", "marks", "info"], delimiter=";")
+            for student in self.students:
+                writer.writerow(student)
+
+
+repository = Repository()
+
+
+def inject_repository(func):
+    def wrapped(*args, **kwargs):
+        return func(*args, **kwargs, repo=repository)
+    return wrapped
+
+
+def generate_id() -> str:
+    students_ids = [int(student["id"]) for student in repository.students]
     new_id = max(students_ids) + 1
-    return new_id
+    return str(new_id)
 
 
-def add_student(student: dict) -> dict:
-    if not student.get("name"):
-        print("==============================\n"
-              "Enter at least the student Name ")
-        while not student.get("name"):
-            student = ask_student_payload()
-        storage.append(student)
-        return student
-    else:
-        # action
-        storage.append(student)
-        return student
+class StudentService:
 
+    @inject_repository
+    def add_student(self, student: dict, repo: Repository = None) -> dict:
+        if not student.get("name"):
+            print("==============================\n"
+                  "Enter at least the student Name ")
+            while not student.get("name"):
+                student = ask_student_payload()
+            repo.students.append(student)
+            return student
+        else:
+            # action
+            repo.students.append(student)
+            return student
 
-def add_marks(target_id: int, raw_marks: str) -> dict | None:
-    """
-    Function to add Marks
-    """
-    for index, target in enumerate(storage):
-        if target_id == target["id"]:
-            marks_to_add: list = [int(i) for i in raw_marks.replace(" ", "").split(",") if i]
-            storage[index]['marks'].extend(marks_to_add)
-            return target
+    @inject_repository
+    def add_marks(self,  target_id: str, raw_marks: str, repo: Repository = None) -> dict | None:
+        """
+        Function to add Marks
+        """
+        for index, target in enumerate(repo.students):
+            if target_id == target["id"]:
+                if repo.students[index]["marks"] == "No marks":
+                    repo.students[index]["marks"] = raw_marks
+                else:
+                    repo.students[index]["marks"] += "," + raw_marks
+                return target
 
+    @inject_repository
+    def update_student_data(self, student_id: str, repo: Repository = None) -> dict | None:
+        """
+        Function to update student data
+        """
+        def change_name(name_for_update):
+            repo.students[index]["name"] = name_for_update
+            repo.students[index]["marks"] = ""
+            repo.students[index]["info"] = ""
+            print("Name is Updated, marks and info erased due to new Name")
 
-def update_student_data(student_id: int) -> dict | None:
-    """
-    Function to update student data
-    """
-    def change_name():
-        storage[index]["name"] = new_name
-        storage[index]["marks"] = []
-        storage[index]["info"] = ""
-        print("Name is Updated, marks and info erased due to new Name")
-
-    def additional_marks():
-        try:
-            storage[index]["marks"] = [int(item) for item in marks.replace(" ", "").split(",")]
-        except ValueError:
-            print("Invalid input, Marks will be saved as Empty")
-            storage[index]["marks"] = []
+        def additional_marks(marks_to_add):
+            if repo.students[index]["marks"] == "No marks":
+                repo.students[index]["marks"] = marks_to_add
+            else:
+                repo.students[index]["marks"] += "," + marks_to_add
             return None
 
-    def change_info():
-        current_info: str = storage[index]["info"]
-        info_match = fuzz.token_sort_ratio(new_info, current_info)
-        if info_match < 50:
-            storage[index]["info"] += ", " + new_info
+        def change_info(info_to_update):
+            current_info: str = repo.students[index]["info"]
+            if current_info == "No info":
+                current_info = ""
+            info_match = fuzz.token_sort_ratio(info_to_update, current_info)
+            if info_match < 50:
+                if current_info:
+                    repo.students[index]["info"] += ", " + new_info
+                else:
+                    repo.students[index]["info"] = new_info
+            else:
+                repo.students[index]["info"] = new_info
+
+        if student_id not in [ids['id'] for ids in repository.students]:
+            return None
         else:
-            storage[index]["info"] = new_info
-
-    if student_id not in [ids['id'] for ids in storage]:
-        return None
-    else:
-        for index, target in enumerate(storage):
-            if student_id == target["id"]:
-                print(f"What you Like to update for {storage[index]['name']}?\n"
-                      f"{'For Name Enter  ':<25} '1'\n"
-                      f"{'For Marks Enter  ':<25} '2'\n"
-                      f"{'For Info Enter  ':<25} '3'\n"
-                      f"{'For Marks and Info Enter':<25} '4'\n"
-                      f"===========================")
-                item_to_update = input("Please select :")
-                if item_to_update == "1":
-                    new_name: str = input("Please Enter a NEW Name :")
-                    if new_name:
-                        change_name()
-                    else:
+            for index, target in enumerate(repo.students):
+                if student_id == target["id"]:
+                    print(f"What you Like to update for {repo.students[index]['name']}?\n"
+                          f"{'For Name Enter  ':<25} '1'\n"
+                          f"{'For Marks Enter  ':<25} '2'\n"
+                          f"{'For Info Enter  ':<25} '3'\n"
+                          f"{'For Marks and Info Enter':<25} '4'\n"
+                          f"===========================")
+                    item_to_update = input("Please select :")
+                    if item_to_update == "1":
+                        new_name: str = input("Please Enter a NEW Name :")
+                        if new_name:
+                            change_name(new_name)
+                        else:
+                            return None
+                    elif item_to_update == "2":
+                        marks = input("Please Input a new Marks, separated by ',' :")
+                        if marks:
+                            additional_marks(marks)
+                            print("Marks Added")
+                        else:
+                            return None
+                    elif item_to_update == "3":
+                        new_info: str = input("Please Enter a NEW Info :")
+                        if new_info:
+                            change_info(new_info)
+                        else:
+                            return None
+                    elif item_to_update == "4":
+                        marks = input("Please Input a new Marks, separated by ',' :")
+                        if marks:
+                            additional_marks(marks)
+                        else:
+                            return None
+                        new_info: str = input("Please Enter a NEW Info :")
+                        if new_info:
+                            change_info(new_info)
+                        else:
+                            return None
+                    elif item_to_update == "":
                         return None
-                elif item_to_update == "2":
-                    marks = input("Please Input a new Marks, separated by ',' :")
-                    if marks:
-                        additional_marks()
-                    else:
-                        return None
-                elif item_to_update == "3":
-                    new_info: str = input("Please Enter a NEW Info :")
-                    if new_info:
-                        change_info()
-                    else:
-                        return None
-                elif item_to_update == "4":
-                    marks = input("Please Input a new Marks, separated by ',' :")
-                    if marks:
-                        additional_marks()
-                    else:
-                        return None
-                    new_info: str = input("Please Enter a NEW Info :")
-                    if new_info:
-                        change_info()
-                    else:
-                        return None
+                    return repo.students[index]
 
-                return storage[index]
+    @inject_repository
+    def show_students(self, repo: Repository = None):
+        print("=========================\n")
+        for student in repo.students:
+            print(f"{student['id']}. Student {student['name']}\n")
+        print("=========================\n")
 
+    @inject_repository
+    def search_student(self, student_id: str, repo: Repository = None) -> None:
 
-def show_students():
-    print("=========================\n")
-    for student in storage:
-        print(f"{student['id']}. Student {student['name']}\n")
-    print("=========================\n")
+        for student in repo.students:
+            info = (
+                "=========================\n"
+                f"[{student['id']}] Student {student['name']}\n"
+                f"Marks: {student['marks']}\n"
+                f"Info: {student['info']}\n"
+                "=========================\n"
+            )
 
+            if student["id"] == student_id:
+                print(info)
+                return
 
-def search_student(student_id: int) -> None:
-    for student in storage:
-        info = (
-            "=========================\n"
-            f"[{student['id']}] Student {student['name']}\n"
-            f"Marks: {student['marks']}\n"
-            f"Info: {student['info']}\n"
-            "=========================\n"
-        )
+        print(f"Student {student_id} not found")
 
-        if student["id"] == student_id:
-            print(info)
-            return
-
-    print(f"Student {student_id} not found")
+    @inject_repository
+    def delete_student(self, student_id: str, repo: Repository = None):
+        print("=========================\n")
+        for index, student in enumerate(repo.students):
+            if student_id == student["id"]:
+                name_to_delete = student['name']
+                del repo.students[index]
+                print(f"Student {name_to_delete} Deleted\n")
+        print("=========================\n")
 
 
 def ask_student_payload() -> dict:
     print("==============================")
     name = input("Please Enter The student Name : ")
     marks = input("Please Enter the Student marks if it exist separated by ',' :")
-    if marks:
-        try:
-            marks = [int(item) for item in marks.replace(" ", "").split(",")]
-        except ValueError as err:
-            print("Invalid input, Marks will be saved as Empty")
-            marks = []
-    else:
-        marks = []
+    if not marks:
+        marks = "No marks"
     info = input("Please Enter the additional information if applicable :")
     if not info:
-        info = ""
+        info = "No info"
 
     return {
         "id": generate_id(),
@@ -215,47 +199,57 @@ def ask_student_payload() -> dict:
 
 
 def student_management_command_handle(command: str):
-    if command == "show":
-        show_students()
+    student_service = StudentService()
+    if command == "show_students":
+        student_service.show_students()
     elif command == "add_student":
         data = ask_student_payload()
         if data:
-            student: dict = add_student(data)
-            print(f"Student: {student['name']} is added with ID {student['id']}")
+            student_service.add_student(data)
+            print(f"Student: {data['name']} is added with ID {data['id']}")
+            repository.update_storage()
         else:
             print("The student's data is NOT correct. Please try again")
-    elif command == "search":
+    elif command == "search_student":
+        student_service.show_students()
         student_id: str = input("\nEnter student's ID: ")
         if student_id:
-            search_student(student_id=int(student_id))
+            student_service.search_student(student_id=student_id)
         else:
             print("Student's name is required to search")
     elif command == "add_marks":
-        show_students()
-        target_id = int(input("Please Enter The Student ID to add marks:"))
-        if target_id in [ids['id'] for ids in storage]:
+        student_service.show_students()
+        target_id = input("Please Enter The Student ID to add marks:")
+        if target_id in [ids['id'] for ids in repository.students]:
             raw_marks = input("Please input the marks to add separated by ',':")
-            target = add_marks(target_id, raw_marks)
+            target = student_service.add_marks(target_id, raw_marks)
+            repository.update_storage()
             print(f"{target['name']} Marks added...")
         else:
             print("Student not Found")
-    elif command == "update":
-        show_students()
-        try:
-            student_id: int = int(input("Please Enter Student ID to UPDATE: "))
-            if student_id:
-                updated_student = update_student_data(student_id)
-                if updated_student:
-                    print(f"{updated_student['name']} updated accordingly ...")
-                else:
-                    print("Error to Update")
-        except ValueError:
-            print("Error to Update")
+    elif command == "update_student":
+        student_service.show_students()
+        student_id: str = input("Please Enter Student ID to UPDATE: ")
+        if student_id:
+            updated_student = student_service.update_student_data(student_id)
+            if updated_student:
+                print(f"{updated_student['name']} updated accordingly ...")
+                repository.update_storage()
+            else:
+                print("Error to Update")
+    elif command == "delete_student":
+        student_service.show_students()
+        student_id: str = input("Please Enter Student ID to DELETE: ")
+        if student_id in [ids['id'] for ids in repository.students]:
+            student_service.delete_student(student_id)
+            repository.update_storage()
+        else:
+            print("Student not Found")
 
 
 def main():
     OPERATIONAL_COMMANDS = ("quit", "help")
-    STUDENT_MANAGEMENT_COMMANDS = ("show", "add_student", "search", "add_marks", "update")
+    STUDENT_MANAGEMENT_COMMANDS = ("show_students", "add_student", "search_student", "add_marks", "update_student", "delete_student")
     AVAILABLE_COMMANDS = (*OPERATIONAL_COMMANDS, *STUDENT_MANAGEMENT_COMMANDS)
 
     HELP_MESSAGE = (
